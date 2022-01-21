9bc254e974b81631d479d2eb1ac52c4ac605cedf
Paul Nuffer <pnuffer@pm.me>
2022-01-21 06:34:31 -0700

    add test for boundaries

--- dice.go
+++ dice.go
@@ -4,27 +4,32 @@ import (
 	"crypto/rand"
 	"fmt"
 	"math/big"
+	"os"
 )
 
 // SixSided returns a random value between 1 and 6
-func SixSided() int {
+func SixSided() (int, error) {
 	r, err := rand.Int(rand.Reader, big.NewInt(int64(6)))
 	if err != nil {
-		return 0
+		fmt.Printf("Error calling rand.Int\n")
+		return 0, err
 	}
-	return 1+int(r.Int64())
+	if int(r.Int64()) < 0 || int(r.Int64()) > 5 {
+		fmt.Printf("Error: die has unexpected number of sides\n")
+		return 0, err
+	}
+
+	return 1+int(r.Int64()), err
 }
 
 
 func main() {
 	var results []int
 	for i := 0; i < 5; i++ {
-		r := SixSided()
-		if r == 0 {
-			fmt.Printf("Error creating a random number\n")
-		}
-		if r > 6 {
-			fmt.Printf("\n\nError, roll is > 6: %d\n\n", r)
+		r, err := SixSided()
+		if err != nil {
+			fmt.Printf("Problem creating a random number.\n")
+			os.Exit(1)
 		}
 		results = append(results, r)
 	}
--- dice_test.go
+++ dice_test.go
@@ -4,9 +4,17 @@ import (
 	"testing"
 )
 
-// BenchmarkRandInt
+// BenchmarkSixSided
 func BenchmarkSixSided(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		SixSided()
+	}
+}
+
+// TestSixSidedBoundaries verifies SixSided returns between 1 and 6, inclusive.
+func TestSixSidedBoundaries(t *testing.T) {
+	r := SixSided()
+	if r <= 0 || r > 6 {
+		t.Fatalf("SixSided() returned a value out of bounds: %d\n", r)
 	}
 }

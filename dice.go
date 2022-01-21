13e45e33a1adb8b78ab0c01f6bd171dd5f19fc52
Paul Nuffer <pnuffer@pm.me>
2022-01-21 06:17:58 -0700

    basic 6 sided roller

--- dice.go
+++ dice.go
@@ -1,14 +1,38 @@
-package dice
+package main
 
 import (
-	"crypt/rand"
+	"crypto/rand"
+	"fmt"
 	"math/big"
 )
 
-func RandInt(min, max int) int {
-	r, err := rand.Int(rand.Reader, big.NewInt(int64(max-min)))
+// SixSided returns a random value between 1 and 6
+func SixSided() int {
+	r, err := rand.Int(rand.Reader, big.NewInt(int64(6)))
 	if err != nil {
 		return 0
 	}
-	return min + int(r.int64())
+	return 1+int(r.Int64())
+}
+
+
+func main() {
+	var results []int
+	for i := 0; i < 5; i++ {
+		r := SixSided()
+		if r == 0 {
+			fmt.Printf("Error creating a random number\n")
+		}
+		if r > 6 {
+			fmt.Printf("\n\nError, roll is > 6: %d\n\n", r)
+		}
+		fmt.Printf("%d\t", r)
+		results = append(results, r)
+	}
+	fmt.Printf("\ndone\n")
+
+	for _, r := range results {
+		fmt.Printf("%d", r)
+	}
+	fmt.Printf("\n")
 }
